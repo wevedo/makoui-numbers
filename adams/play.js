4a1c6251ba142275cb4b1cb7f524c1cd4c6db331
wevedo <mribraahtech@gmail.com>
2025-07-15 22:54:06 +0300

    Update play.js

--- adams/play.js
+++ adams/play.js
@@ -7,12 +7,8 @@ const apiKey = 'gifted_api_6kuv56877d';
 
 const audioApis = [
     // David Cyril APIs first for faster MP3
-   // `https://apis.davidcyriltech.my.id/download/ytmp3?url=`,
-   // `https://apis.davidcyriltech.my.id/youtube/mp3?url=`,
-    `https://api.giftedtech.web.id/api/download/ytdlv2?apikey=${apiKey}&url=`,
-    `https://api.giftedtech.web.id/api/download/ytdl?apikey=${apiKey}&url=`,
-    `https://api.giftedtech.web.id/api/download/ytaudio?apikey=${apiKey}&format=128kbps&url=`,
-    `https://api.giftedtech.web.id/api/download/yta?apikey=${apiKey}&url=`
+    `https://apis.davidcyriltech.my.id/download/ytmp3?url=`,
+    `https://apis.davidcyriltech.my.id/youtube/mp3?url=`
 ];
 
 const videoApis = [
